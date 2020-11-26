2b2bdfff74116a3af179515135de042081f6f8be
Greg Harner <accounts@yongsa.net>
2020-11-26 08:37:46 -0500

    post

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const firebase = {
+	production: false,
+	Cfg: {
+		apiKey: 'AIzaSyDbsI5G0Rs4jIwujTQ0ZaL_lRdF3s3IqfI',
+		authDomain: 'partnerineducation-0.firebaseapp.com',
+		databaseURL: 'https://partnerineducation-0.firebaseio.com',
+		projectId: 'partnerineducation-0',
+		storageBucket: 'partnerineducation-0.appspot.com',
+		messagingSenderId: '838704600847',
+		appId: '1:838704600847:web:583eef68a7eec697b2107a'
+	},
+	PayPal_Client_Id: 'AT7N55QgBstZFh1VPSXCsGLt2NBi94EOEpIthf3eVhInpePmMw2o7-08iZv_yOeUzpS5jjcmtPFvWAGY'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ src/environments/gapisettings.ts
@@ -0,0 +1,8 @@
+export const gapiSettings = {
+	Cfg: {
+		apiKey: 'AIzaSyA54qDZe_fPdpWLOuLocfaxu_9AyxOO6nQ',
+		clientId: '838704600847-ce4gakjcccqhsk3qji1nmq9n3fq7rdm0.apps.googleusercontent.com',
+		discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
+		scope: 'https://www.googleapis.com/auth/user.emails.read ' + 'https://www.googleapis.com/auth/userinfo.profile ',
+	},
+};
