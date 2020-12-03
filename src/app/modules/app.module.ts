c7ace0bf2b2f777ce66661c4182f0c97aa0224c5
Greg Harner <accounts@yongsa.net>
2020-12-03 05:40:45 -0500

    post

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from '../app.component';
 import { RxJsDemo } from '../components/rxjs-demo/rxjs-demo.component';
 import { ObjDemoComponent } from '../components/obj-demo/obj-demo.component';
+import { ContentProjectionComponent } from '../components/content-projection/content-projection.component';
 
 @NgModule({
 	imports: [BrowserModule, FormsModule],
-	declarations: [AppComponent, RxJsDemo, ObjDemoComponent],
+	declarations: [AppComponent, RxJsDemo, ObjDemoComponent, ContentProjectionComponent],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
